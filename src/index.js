a0d98ef6198101eedac6179be9d744e9518e406c
Yuliia-Gorgodze <taragorgodze@gmail.com>
2020-12-13 12:07:34 +0200

    homework done

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const refs = {
+    buttunStart : document.querySelector('[data-action="start"]'),
+    buttunStop : document.querySelector('[data-action="stop"]'),
+    body: document.querySelector('body')
+}
+
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
+
+refs.buttunStart.addEventListener('click', changeBgColor);
+refs.buttunStop.addEventListener('click', stopChangeBgColor)
+
+let buttunStartActive = true;
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+function changeBgColor () {
+        if(buttunStartActive){
+            timeOutId  = setInterval(() => {
+                 const randomColor = randomIntegerFromInterval(0, colors.length);
+                 refs.body.style.backgroundColor = colors[randomColor];
+                 buttunStartActive = false;
+             }, 1000)
+        }      
+}
+ 
+function stopChangeBgColor () {
+  clearTimeout(timeOutId)
+  buttunStartActive = true;
+}
+
+
+
